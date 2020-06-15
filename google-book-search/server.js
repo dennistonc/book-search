8e7191e0981d6518a6d51250ad628735ec6379be
dennistonc <dennistonc@gmail.com>
2020-06-15 00:23:14 -0500

    fix in server.js

--- google-book-search/server.js
+++ google-book-search/server.js
@@ -11,8 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("public"));
-  app.use(express.static("src"));
+  app.use(express.static("client/build"));
 }
 
 
